d63c783abf5124ea635d933b0f1302984b404765
AnthonyStseshyts <a.stseshyts@mail.ru>
2019-08-16 14:10:31 +0300

    deleted one  console.log in index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,9 @@ import onLoad from "./app/DOM/onload";
 import generateId from "./app/utils/generateID";
 import generateName from "./app/utils/generateName";
 import generatingOrder from "./app/utils/generatingOrder";
-
 import inDomClient from "./app/DOM/inDomClient";
-
 import onClientFunction from "./app/auxiliary function/onClient";
-
 import randomInteger from "./app/utils/randomInteger";
-
 import {CLIENTS_ORDERS} from "./app/constants/const";
 import {COOKS_ORDER} from "./app/constants/const";
 import {COOKING_ORDERS} from "./app/constants/const";
@@ -18,17 +14,12 @@ import {DONE_ORDERS} from "./app/constants/const";
 import {firstNames} from "./app/constants/const";
 import {secondNames} from "./app/constants/const";
 import {INGREDIENTS} from "./app/constants/const";
-
 import css from "./css/style.css";
 
-console.log(css.toString());
 const clientGenerator = new ClientGenerator();
 const cookGenrator = new CookGenerator();
 
-
 onLoad();
-
-
 setTimeout(function addClients() {
     CLIENTS_ORDERS.push(
       new ClientGenerator(
